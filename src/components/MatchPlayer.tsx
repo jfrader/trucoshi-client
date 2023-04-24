7ba5e8d6a503a0c31ced405123d6c9f77d8972f5
J Francisco Rader <franciscorader@gmail.com>
2023-04-24 15:32:44 -0300

    add turn timer

--- src/components/MatchPlayer.tsx
+++ src/components/MatchPlayer.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, ButtonGroup } from "@mui/material";
-import { ICard, IPublicPlayer } from "trucoshi";
+import { Box, Button, ButtonGroup, LinearProgress } from "@mui/material";
+import { ICard, IPublicPlayer, PLAYER_ABANDON_TIMEOUT, PLAYER_TURN_TIMEOUT } from "trucoshi";
 import { useRounds } from "../trucoshi/hooks/useRounds";
 import { ITrucoshiMatchActions, ITrucoshiMatchState } from "../trucoshi/types";
 import { GameCard } from "./GameCard";
 import { PlayerTag } from "./PlayerTag";
 import { DANGEROUS_COMMANDS, COMMANDS_HUMAN_READABLE } from "../trucoshi/constants";
+import { useEffect, useState } from "react";
 
 type PlayerProps = Pick<ITrucoshiMatchState, "canPlay" | "canSay" | "previousHand" | "match"> & {
   player: IPublicPlayer;
@@ -25,8 +26,48 @@ export const MatchPlayer = ({
 
   const bestEnvido = Math.max(...(player.envido || []));
 
+  const [turnTimer, setTurnTimer] = useState<{ isExtension: boolean; progress: number }>({
+    isExtension: false,
+    progress: 0,
+  });
+
+  useEffect(() => {
+    if (!player.isTurn) {
+      return
+    }
+    const interval = setInterval(() => {
+      setTurnTimer(({ isExtension }) => {
+        const now = Math.floor(Date.now());
+        if (!player.turnExpiresAt || !player.turnExtensionExpiresAt) {
+          return { isExtension: false, progress: 0 };
+        }
+        if (isExtension) {
+          const difference = player.turnExtensionExpiresAt - now;
+          const progress = Math.floor((difference * 100) / PLAYER_ABANDON_TIMEOUT)
+          return { isExtension, progress };
+        }
+        const difference = player.turnExpiresAt - now;
+        if (difference > 0) {
+          const progress = Math.floor((difference * 100) / PLAYER_TURN_TIMEOUT)
+          return {
+            isExtension: false,
+            progress,
+          };
+        }
+        return { isExtension: true, progress: 100 };
+      });
+    }, 16);
+
+    return () => clearInterval(interval);
+  }, [player]);
+
   return (
     <Box maxWidth="100%" pt={1} display="flex" flexDirection="column" flexGrow={1} height="100%">
+      <LinearProgress
+        variant="determinate"
+        color={turnTimer.isExtension ? "error" : "warning"}
+        value={turnTimer.progress}
+      />
       <PlayerTag player={player} isTurn={player.isTurn} />
       <Box pt={1}>
         {!isPrevious &&
