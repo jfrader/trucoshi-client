3d744ecd9b968a9b21a34fd977ea3660d13e15e4
J Francisco Rader <franciscorader@gmail.com>
2024-01-30 17:35:16 -0300

    fix missing value

--- src/trucoshi/constants.ts
+++ src/trucoshi/constants.ts
@@ -16,15 +16,16 @@ export const DANGEROUS_COMMANDS: ECommand[] = [
 
 export const COMMANDS_HUMAN_READABLE: Record<ECommand, string> = {
   [ESayCommand.MAZO]: "Mazo",
-  [ETrucoCommand.TRUCO]: "Truco",
-  [ETrucoCommand.RE_TRUCO]: "Re-truco",
-  [ETrucoCommand.VALE_CUATRO]: "Vale cuatro",
+  [ETrucoCommand.TRUCO]: "Truco!",
+  [ETrucoCommand.RE_TRUCO]: "Re-truco!",
+  [ETrucoCommand.VALE_CUATRO]: "Vale cuatro!",
   [EEnvidoCommand.ENVIDO]: "Envido",
   [EEnvidoCommand.REAL_ENVIDO]: "Real envido",
   [EEnvidoCommand.FALTA_ENVIDO]: "Falta envido",
   [EEnvidoAnswerCommand.SON_BUENAS]: "Son buenas",
   [EAnswerCommand.QUIERO]: "Quiero",
   [EAnswerCommand.NO_QUIERO]: "No quiero",
-  [EFlorCommand.FLOR]: "Flor",
-  [EFlorCommand.CONTRAFLOR]: "Contraflor",
+  [EFlorCommand.FLOR]: "Flor!",
+  [EFlorCommand.CONTRAFLOR]: "Contraflor!",
+  [EFlorCommand.ACHICO]: "Con flor me achico"
 };
