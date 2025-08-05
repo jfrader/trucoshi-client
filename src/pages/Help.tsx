7e4abdc9fca1c774ac9b946196809f92fa996312
fran <jfrader@pm.me>
2025-08-05 17:51:15 -0300

    add cards truco ranking

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -6,13 +6,25 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Stack,
   Typography,
 } from "@mui/material";
 import { Link } from "../shared/Link";
 import { GITHUB_LINK_FRAN, HELP_LINKS } from "../assets/links/links";
 import { PageContainer } from "../shared/PageContainer";
+import groupBy from "lodash.groupby";
+import { CARDS, ICard } from "trucoshi";
+import { GameCard } from "../components/card/GameCard";
+import { useTrucoshi } from "../trucoshi/hooks/useTrucoshi";
+import { HandCardContainer } from "../components/card/HandCardContainer";
 
 export const Help = () => {
+  const [, { inspectCard }] = useTrucoshi();
+  const groupedCards = groupBy(
+    Object.entries(CARDS),
+    ([, value]: [ICard, number]) => value
+  ) as Record<string, Array<[ICard, number]>>;
+
   return (
     <PageContainer title="Ayuda" icon={<HelpOutlined fontSize="large" />}>
       <Card>
@@ -33,6 +45,36 @@ export const Help = () => {
               );
             })}
           </List>
+          <Stack pt={2} direction="row" flexWrap="wrap" gap={4}>
+            <Typography variant="caption" fontWeight="bold" fontSize="large" width="100%">
+              Ranking de Cartas en el Truco
+            </Typography>
+            {Object.entries(groupedCards)
+              .sort(([a], [b]) => Number(b) - Number(a))
+              .map(([value, cards], i) => {
+                return (
+                  <Stack gap={2} direction="row" key={value}>
+                    <Typography>{i + 1}. </Typography>
+                    {cards.map(([c], j) => (
+                      <HandCardContainer
+                        open={false}
+                        sx={{
+                          position: "relative",
+                          left: "initial",
+                          right: "initial",
+                          marginLeft: `calc(-1.2em * ${j})`,
+                        }}
+                        cards={cards.length}
+                        i={j}
+                        key={c}
+                      >
+                        <GameCard onClick={() => inspectCard(c)} card={c} />
+                      </HandCardContainer>
+                    ))}
+                  </Stack>
+                );
+              })}
+          </Stack>
           <Typography pt={4} display="block" variant="caption">
             Made with ❤️ by{" "}
             <Link target="_blank" to={GITHUB_LINK_FRAN.to}>
