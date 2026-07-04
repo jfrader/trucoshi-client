fbaa242b8315ff9824f311fdfd353e0dd91fd92e
fran <jfrader@pm.me>
2026-07-04 19:39:48 -0300

    bump

--- src/shared/TrucoshiResponsiveLogoLink.tsx
+++ src/shared/TrucoshiResponsiveLogoLink.tsx
@@ -3,14 +3,31 @@ import { Link } from "./Link";
 import { TrucoshiText } from "./TrucoshiText";
 import { TrucoshiLogo } from "./TrucoshiLogo";
 
-export const TrucoshiResponsiveLogoLink = ({ size = 26 }: { size?: string | number }) => {
+export const TrucoshiResponsiveLogoLink = ({
+  width,
+  height,
+}: {
+  width?: string | number;
+  height?: string | number;
+}) => {
   return (
     <Link to="/" lineHeight={4}>
-      <Typography maxWidth={size} display={{ xs: "none", sm: "block" }} height={size} variant="h6">
-        <TrucoshiText height={size} />
+      <Typography
+        maxWidth={width}
+        display={{ xs: "none", sm: "block" }}
+        height={height || 26}
+        width={width || 80}
+        variant="h6"
+      >
+        <TrucoshiText height={height || 26} width={width || 80} />
       </Typography>
-      <Typography display={{ xs: "block", sm: "none" }} height={size} variant="h6">
-        <TrucoshiLogo height={size} width={size} />
+      <Typography
+        display={{ xs: "block", sm: "none" }}
+        height={height || 30}
+        width={width || 30}
+        variant="h6"
+      >
+        <TrucoshiLogo height={height || 30} width={width || 30} />
       </Typography>
     </Link>
   );
